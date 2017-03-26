93ba9d372ee38e045687c177747e6673a0bedec7
Ashiikgp <vshlkumar56@gmail.com>
2017-03-27 00:10:44 +0530

    Basic GPS Service connection

--- app/src/main/java/ash/quadmap_android/ConnectScreen.java
+++ app/src/main/java/ash/quadmap_android/ConnectScreen.java
@@ -1,13 +1,57 @@
 package ash.quadmap_android;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConnectScreen extends AppCompatActivity {
 
+    private Button connect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect_screen);
+
+        connect = (Button)findViewById(R.id.connect_btn);
+        if(!runtime_permissions()){
+            enableButton();
+        }
+    }
+
+    private void enableButton(){
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),GPSProvider.class);
+                startService(i);
+            }
+        });
+    }
+    private boolean runtime_permissions() {
+        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},100);
+
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 100){
+            if( grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                enableButton();
+            }else {
+                runtime_permissions();
+            }
+        }
     }
 }
