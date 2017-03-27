f707ae9c0ee29207430409eed968fde9b7a4a539
Ashiikgp <vshlkumar56@gmail.com>
2017-03-27 14:44:59 +0530

    Integrate Map and Marker click listner

--- app/src/main/java/ash/quadmap_android/Interface.java
+++ app/src/main/java/ash/quadmap_android/Interface.java
@@ -1,21 +1,27 @@
 package ash.quadmap_android;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.io.BufferedWriter;
+
 public class Interface extends AppCompatActivity {
 
+    private Location Home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interface);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Fragment map = new MapsActivity();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +30,11 @@ public class Interface extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        getSupportFragmentManager().beginTransaction().replace(R.id.map,map).addToBackStack(null).commit();
+        Bundle bundle = getIntent().getExtras();
+        BufferedWriter bw = bundle.getParcelable("Writer");
     }
-
+    public void setHome(Location location){
+        Home = location;
+    }
 }
--- app/src/main/java/ash/quadmap_android/MapsActivity.java
+++ app/src/main/java/ash/quadmap_android/MapsActivity.java
@@ -1,45 +1,69 @@
 package ash.quadmap_android;
 
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.auth.GooglePlayServicesAvailabilityException;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends Fragment {
+public class MapsActivity extends Fragment implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener,
+        GoogleMap.OnMarkerClickListener,
+        GoogleMap.OnInfoWindowClickListener{
 
-    MapView mapView;
-    GoogleMap map;
+    private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    Location location;
+    LocationRequest mLocationRequest;
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.content_interface, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View v = inflater.inflate(R.layout.activity_maps, container, false);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1000);
 
-        mapView = (MapView) v.findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-
-        map = mapView.getMapAsync(new OnMapReadyCallback() {
+        mGoogleApiClient = new GoogleApiClient.Builder(getContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-                map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                map.getUiSettings().setMyLocationButtonEnabled(true);
-                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                mMap = googleMap;
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     // TODO: Consider calling
                     //    ActivityCompat#requestPermissions
                     // here to request the missing permissions, and then overriding
@@ -49,18 +73,87 @@ public class MapsActivity extends Fragment {
                     // for ActivityCompat#requestPermissions for more details.
                     return;
                 }
-                map.setMyLocationEnabled(true);
-                try{
-                    MapsInitializer.initialize(getActivity());
+                mMap.setMyLocationEnabled(true);
 
-                }
-                CameraUpdate cam = CameraUpdateFactory.newLatLngZoom(new LatLng(22,87),10);
-                map.animateCamera(cam);
+                // Add a marker in Sydney and move the camera
+                LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(10.0f).build();
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+                mMap.moveCamera(cameraUpdate);
+                mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLng latLng) {
+                        mMap.clear();
+                        MarkerOptions options = new MarkerOptions()
+                                .position(latLng)
+                                .title("Tap to send quad");
+                        mMap.addMarker(options);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    }
+                });
             }
         });
-
         return v;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i("API", "Location services connected.");
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i("API", "Location services suspended.");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i("API", "Location services failed.");
+    }
+
+    @Override
+    public void onLocationChanged(Location _location) {
+        location = _location;
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Log.i("Marker","Click Marker");
+        return true;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Log.i("Info","Info Marker");
+    }
 }
